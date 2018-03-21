9df5e1720d5caef20a56545dde66569f37ea10f9
Ioan-PC\Ioan <ioan_voiculescu@icloud.com>
2018-03-22 00:08:23 +0200

    no message

--- Lab1/src/Constants.java
+++ Lab1/src/Constants.java
@@ -3,10 +3,5 @@
  */
 public class Constants
 {
-    public static int maxNumberGenerated = 100;
-    public static int queueSize = 10;
-    public static int sleepTimeConsumer = 350;
-    public static int sleepTimeProducer = 180;
-    public static int numberOfConsumers = 14;
-    public static int numberOfProducer = 17;
+    public static int sectionValue = 7;
 }
