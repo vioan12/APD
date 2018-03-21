61836dc549bb8ccc0819503a3a30c01b2b4b45cb
Ioan-PC\Ioan <ioan_voiculescu@icloud.com>
2018-03-22 00:00:29 +0200

    no message

--- Lab1/src/Generator.java
+++
@@ -1,7 +0,0 @@
-/**
- * Created by ioan on 3/3/18.
- */
-public interface Generator
-{
-    int next();
-}
--- Lab1/src/Main.java
+++ Lab1/src/Main.java
@@ -1,39 +1,13 @@
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import mpi.*;
 
 public class Main
 {
-    private static Thread[] producerThread, consumerThread;
-    private static ProducerConsumer producerConsumer;
-    private static Object isEmpty, isFull;
     public static void main(String[] args)
     {
-        isEmpty = new Object();
-        isFull = new Object();
-        producerConsumer = new ProducerConsumer(Constants.queueSize);
-        producerThread = new Thread[Constants.numberOfProducer];
-        consumerThread = new Thread[Constants.numberOfConsumers];
-        for (int i = 0; i < producerThread.length; i++) {
-            producerThread[i] = new ProducerThread(producerConsumer, isEmpty, isFull);
-        }
-        for (int i = 0; i < consumerThread.length; i++) {
-            consumerThread[i] = new ConsumerThread(producerConsumer, isEmpty, isFull);
-        }
-        for (int i = 0; i < producerThread.length; i++) {
-            producerThread[i].start();
-        }
-        for (int i = 0; i < consumerThread.length; i++) {
-            consumerThread[i].start();
-        }
-        try {
-            for (int i = 0; i < producerThread.length; i++) {
-                producerThread[i].join();
-            }
-            for (int i = 0; i < consumerThread.length; i++) {
-                consumerThread[i].join();
-            }
-        } catch (Exception exception) {
-            System.out.println(exception.getMessage());
-        }
+        MPI.Init(args);
+        int myrank = MPI.COMM_WORLD.Rank();
+        int size = MPI.COMM_WORLD.Size();
+        System.out.println("Hello world from rank " + myrank + " of " + size);
+        MPI.Finalize();
     }
 }
